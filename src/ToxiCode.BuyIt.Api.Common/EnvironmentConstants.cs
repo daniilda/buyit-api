873b9cc2db0c92fa02dd1729ea30eae1a3c60fdb
daniilda <daniilda@hotmail.com>
2022-05-09 12:01:57 +0300

    test

--- src/ToxiCode.BuyIt.Api.Common/EnvironmentConstants.cs
+++ src/ToxiCode.BuyIt.Api.Common/EnvironmentConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace ToxiCode.BuyIt.Api.Common;
 
-public static class EnvironmentConstants
+public static class EnvironmentConstants 
 {
     public const string IsRunningInContainer = "DOTNET_RUNNING_IN_CONTAINER";
     public const string HostUrl = "HOST_URL";
